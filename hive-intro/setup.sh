8ab74d4fc85c98d2f52eaf682ba535daa963f283
Taufik <profesorgreen36@gmail.com>
2021-11-04 09:16:15 +0700

    hive-intro install java

--- hive-intro/setup.sh
+++ hive-intro/setup.sh
@@ -2,7 +2,9 @@
 
 ## Setup Java 8
 
-apt-get update && apt-get install openjdk-8-jdk -y
+apt-get update -y 
+
+apt-get install openjdk-8-jdk -y
 
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 
