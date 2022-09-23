bfc0c39e354300984b5874d527bfa38653079bc8
Taufik <profesorgreen36@gmail.com>
2022-09-23 16:48:02 +0700

    debug postgres

--- sql-with-postgresql/setup.sh
+++ sql-with-postgresql/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #docker-compose up -d
+
 sudo apt update
 
-sudo apt install postgresql postgresql-contrib
+sudo apt install postgresql postgresql-contrib -y
 
 # sudo -u postgres psql
