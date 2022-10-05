5a084db67f497e71a74c0f9b9ebec70d393ed735
Taufik Maulana <taufikmaulana@pop-os.localdomain>
2022-10-05 17:49:10 +0700

    init

--- nodejs-api/script.sh
+++ nodejs-api/script.sh
@@ -3,10 +3,8 @@
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - 
 sudo apt-get install -y nodejs
 
-git clone https://github.com/sdcilsy/nodejs_api.git
-
 npm install
 
-node server.js
-
 docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=nodejs_api -e MYSQL_DATABASE=nodejs_api -e MYSQL_USER=nodejs_api -e MYSQL_PASSWORD=nodejs_api -d mysql:latest -v /root/dump.sql:/docker-entrypoint-initdb.d/dump.sql
+
+node server.js
