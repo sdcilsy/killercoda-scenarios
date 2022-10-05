35778eeeb50397f4df9a8b7caa8a6adb669c3bb9
Taufik Maulana <taufikmaulana@pop-os.localdomain>
2022-10-05 17:29:22 +0700

    add nodejs api

--- nodejs-api/script.sh
+++ nodejs-api/script.sh
@@ -11,4 +11,4 @@ npm install
 
 node server.js
 
-docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=nodejs-api -d mysql:latest -v /root/dump.sql:/docker-entrypoint-initdb.d/dump.sql
+docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=nodejs_api -e MYSQL_DATABASE=nodejs_api -e MYSQL_USER=nodejs_api -e MYSQL_PASSWORD=nodejs_api -d mysql:latest -v /root/dump.sql:/docker-entrypoint-initdb.d/dump.sql
