60407f8f08b819f7cee948a0320007b94ac0fa00
Taufik Maulana <taufikmaulana@pop-os.localdomain>
2022-10-06 14:11:28 +0700

    add nodejs

--- nodejs-api/script.sh
+++ nodejs-api/script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - 
-sudo apt-get install -y nodejs
 
-npm install
+sudo apt-get install -y nodejs </dev/null
 
 docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=nodejs_api -e MYSQL_DATABASE=nodejs_api -e MYSQL_USER=nodejs_api -e MYSQL_PASSWORD=nodejs_api -d mysql:latest -v /root/dump.sql:/docker-entrypoint-initdb.d/dump.sql
 
